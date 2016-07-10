d8a7d7a052ee8fd1a8be8c74c542f1afe0d59a7a
andela-jwarugu <joy.warugu@andela.com>
2016-07-10 11:02:11 +0300

    Beautify code

--- src/string_manipulation.js
+++ src/string_manipulation.js
@@ -1,18 +1,17 @@
-(function(){
+(function() {
 	'use strict';
 
-	String.prototype.hasVowels = function(){
+	String.prototype.hasVowels = function() {
 		return /[aeiou]/gi.test(this);
 	};
 
-	String.prototype.toUpper = function(){
+	String.prototype.toUpper = function() {
 		var capitalized = '';
-		for (var i=0; i<this.length; i++){
-			if (this.charCodeAt(i)>=97 && this.charCodeAt(i)<=122){
+		for (var i = 0; i < this.length; i++) {
+			if (this.charCodeAt(i) >= 97 && this.charCodeAt(i) <= 122) {
 				var asciiCode = this.charCodeAt(i) - 32;
 				capitalized += String.fromCharCode(asciiCode);
-			}
-			else {
+			} else {
 				capitalized += this[i];
 			}
 
@@ -20,14 +19,13 @@
 		return capitalized;
 	};
 
-	String.prototype.toLower = function(){
+	String.prototype.toLower = function() {
 		var lowercased = '';
-		for (var i=0; i<this.length; i++){
-			if (this.charCodeAt(i)>=65 && this.charCodeAt(i)<=90){
+		for (var i = 0; i < this.length; i++) {
+			if (this.charCodeAt(i) >= 65 && this.charCodeAt(i) <= 90) {
 				var asciiCode = this.charCodeAt(i) + 32;
 				lowercased += String.fromCharCode(asciiCode);
-			}
-			else {
+			} else {
 				lowercased += this[i];
 			}
 
@@ -35,30 +33,30 @@
 		return lowercased;
 	};
 
-	String.prototype.ucFirst = function(){
+	String.prototype.ucFirst = function() {
 		return this.replace(/^[a-z]/, this[0].toUpper());
 	};
 
-	String.prototype.isQuestion = function(){
+	String.prototype.isQuestion = function() {
 		return /[?]$/.test(this);
 	};
 
-	String.prototype.words = function(){
+	String.prototype.words = function() {
 		return this.split(/\W+/);
 	};
-	//
-	String.prototype.wordCount = function(){
+	
+	String.prototype.wordCount = function() {
 		var stringArray = this.words();
 		return stringArray.length;
 	};
 
-	String.prototype.toCurrency = function(){
+	String.prototype.toCurrency = function() {
 		var currencyObj = parseFloat(this).toFixed(2);
 		var re = new RegExp('(\\d)(?=(\\d{3})+\\.)', 'g');
 		return (currencyObj.replace(re, '$1,')).toString();
 	};
 
-	String.prototype.fromCurrency = function(){
+	String.prototype.fromCurrency = function() {
 		return Number(this.replace(/,/g, ''));
 	};
 })();
